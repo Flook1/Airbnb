7931928010a348fb9e3b84574422b0152d97954c
Flook1 <Thanadolpoolyiam@gmail.com>
2023-08-08 12:31:30 +0700

    Auth UI completed

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,27 +3,27 @@
 import { IconType } from "react-icons";
 
 interface ButtonProps {
-    label: string;
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
-    disabled?: boolean;
-    outline?: boolean;
-    small?: boolean;
-    icon?: IconType;
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
 }
 
-const Button: React.FC<ButtonProps> = ({
-    label,
-    onClick,
-    disabled,
-    outline,
-    small,
-    icon: icon
+const Button: React.FC<ButtonProps> = ({ 
+  label, 
+  onClick, 
+  disabled, 
+  outline,
+  small,
+  icon: Icon,
 }) => {
-    return ( 
-        <button 
-        onClick={onClick}
-        disabled={disabled}
-        className={`
+  return ( 
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
         relative
         disabled:opacity-70
         disabled:cursor-not-allowed
@@ -38,23 +38,21 @@ const Button: React.FC<ButtonProps> = ({
         ${small ? 'py-1' : 'py-3'}
         ${small ? 'font-light' : 'font-semibold'}
         ${small ? 'border-[1px]' : 'border-2'}
-        `}
-        
-        >
-            {icon && (
-                <icon 
-                    size={24}
-                    className="
-                    absolute
-                    left-4
-                    top-3
-                    "
-                />
-            )}
-            {label}
-
-        </button>
-     );
+      `}
+    >
+      {Icon && (
+        <Icon
+          size={24}
+          className="
+            absolute
+            left-4
+            top-3
+          "
+        />
+      )}
+      {label}
+    </button>
+   );
 }
  
 export default Button;
